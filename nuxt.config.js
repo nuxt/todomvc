28720d41d0820fa1090bd22d12479bb421692fff
Alexandre Chopin <wikeo65@gmail.com>
2017-01-10 16:37:55 +0100

    add config

---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  head: {
+    title: 'TodoMVC made with Nuxt.js',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', content: "TodoMVC project made with Nuxt.js." }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
+    ]
+  },
+  css: [
+    { src: 'todomvc-app-css/index.css'}
+  ],
+  router: {
+    linkActiveClass: 'selected'
+  }
+}
