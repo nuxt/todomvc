d35eae2f08e3e9f14735a7e3398fbeb077d7bc94
Alexandre Chopin <wikeo65@gmail.com>
2017-01-10 16:37:50 +0100

    add layout

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nuxt/>
+    <footer class="info">
+			<p>Double-click to edit a todo</p>
+			<p>Written with <a href="https://nuxtjs.org">Nuxt.js</a></p>
+			<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+		</footer>
+  </div>
+</template>
