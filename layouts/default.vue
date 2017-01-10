d3330c668494cdb05de7cc1cc605e1be5567000a
Sébastien Chopin <seb@chopin.io>
2017-01-10 18:54:07 +0100

    Update footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,13 @@
     <footer class="info">
 			<p>Double-click to edit a todo</p>
 			<p>Written with <a href="https://nuxtjs.org">Nuxt.js</a></p>
-			<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+			<p><a href="http://todomvc.com">TodoMVC</a></p>
 		</footer>
   </div>
 </template>
+
+<style>
+.info {
+  font-size: 13px;
+}
+</style>
